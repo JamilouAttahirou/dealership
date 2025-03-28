313e3b6a83e168bd86c1fcc9f15fdfe9496aff7b
JamilouAttahirou <145698603+JamilouAttahirou@users.noreply.github.com>
2025-03-28 19:31:27 -0400

    more

--- dealership/settings.py
+++ dealership/settings.py
@@ -80,11 +80,23 @@ WSGI_APPLICATION = 'dealership.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_database',
+        'USER': 'my_user',
+        'PASSWORD': 'my_password',
+        'HOST': 'db',
+        'PORT': '5432'
     }
 }
 
