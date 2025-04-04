6fe148a3fd1a1830f59391b25312ce9695ecd337
jamil <jamilattahir@gmail.com>
2025-04-04 16:54:37 -0400

    more

--- dealership/settings.py
+++ dealership/settings.py
@@ -84,11 +84,23 @@ WSGI_APPLICATION = 'dealership.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': os.environ['DB_PASSWORD_DEALERSHIP'],
+        'HOST': 'switchback.proxy.rlwy.net',
+        'PORT': '5432',
     }
 }
 
